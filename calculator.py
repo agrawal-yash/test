3e48cab507c758b0b8b1266c9eb2d813a92485ad
agrawal-yash <agrawal.yash1992@gmail.com>
2022-05-09 11:20:03 -0400

    adding subtraction function

--- calculator.py
+++ calculator.py
@@ -8,8 +8,12 @@ Created on Mon May  9 10:46:14 2022
 def addition(num_1, num_2):
     return num_1 + num_2
 
+def subtraction(num_1, num_2):
+    return num_1 - num_2
+
 if __name__ == "__main__":
     num_1 = int(input())
     num_2 = int(input())
     
     print ("Summation : ", addition(num_1, num_2))
+    print ("Difference : ", addition(num_1, num_2))
