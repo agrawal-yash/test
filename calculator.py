833464764df332dc7a166a7c2c9b6752447a8aed
agrawal-yash <agrawal.yash1992@gmail.com>
2022-05-09 10:54:02 -0400

    Adding calculator code with addition feature

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  9 10:46:14 2022
+
+@author: agraw
+"""
+
+def addition(num_1, num_2):
+    return num_1 + num_2
+
+if __name__ == "__main__":
+    num_1 = int(input())
+    num_2 = int(input())
+    
+    print ("Summation : ", addition(num_1, num_2))
