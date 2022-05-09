34568809680269b14ec81d1098965fe4b13e0841
agrawal-yash <agrawal.yash1992@gmail.com>
2022-05-09 11:26:31 -0400

    adding multiplication function

--- calculator.py
+++ calculator.py
@@ -11,9 +11,13 @@ def addition(num_1, num_2):
 def subtraction(num_1, num_2):
     return num_1 - num_2
 
+def multiplication(num_1, num_2):
+    return num_1 * num_2
+
 if __name__ == "__main__":
     num_1 = int(input())
     num_2 = int(input())
     
     print ("Summation : ", addition(num_1, num_2))
-    print ("Difference : ", addition(num_1, num_2))
+    print ("Difference : ", subtraction(num_1, num_2))
+    print ("Product : ", multiplication(num_1, num_2))
